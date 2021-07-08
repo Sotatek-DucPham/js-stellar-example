c718a80b51573f6e53a6edf88bf1cfeda3185266
Sotatek-DucPham <duc.pham@sotatek.com>
2021-07-08 14:28:43 +0700

    feat: create asset

---
+++ src/common.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import { Account, Keypair, Server, Transaction } from 'stellar-sdk';
+
+const ISSUER_PRIVATE_KEY = process.env.ISSUER_PRIVATE_KEY || '';
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
+const HORIZON_URL =
+  process.env.HORIZON_URL || 'https://horizon-testnet.stellar.org';
+
+export const server = new Server(HORIZON_URL);
+export const issuerKeyPair = Keypair.fromSecret(ISSUER_PRIVATE_KEY);
+export const keyPair = Keypair.fromSecret(PRIVATE_KEY);
+
+export const getCurrentAccount = async (kp: Keypair) => {
+  const account = await server.loadAccount(kp.publicKey());
+  return new Account(account.accountId(), account.sequenceNumber().toString());
+};
+
+export const submitTransaction = async (transaction: Transaction) => {
+  try {
+    return server.submitTransaction(transaction);
+  } catch (err) {
+    console.log(JSON.stringify(err.response.data, null, 2));
+    throw err;
+  }
+};
---
+++ src/create_asset.ts
@@ -0,0 +1,49 @@
+import { Asset, Networks, Operation, TransactionBuilder } from 'stellar-sdk';
+import {
+  getCurrentAccount,
+  issuerKeyPair,
+  keyPair,
+  server,
+  submitTransaction,
+} from './common';
+
+(async () => {
+  const fee = await server.fetchBaseFee();
+  const asset = new Asset('WETH', issuerKeyPair.publicKey());
+
+  // trust line
+  {
+    const account = await getCurrentAccount(keyPair);
+    const transaction = new TransactionBuilder(account, {
+      fee: fee.toString(),
+      networkPassphrase: Networks.TESTNET,
+    })
+      .addOperation(Operation.changeTrust({ asset }))
+      .setTimeout(30)
+      .build();
+    transaction.sign(keyPair);
+    const tx = await server.submitTransaction(transaction);
+    console.log(`Trusted, tx=${tx.hash}`);
+  }
+
+  // create asset
+  {
+    const issuerAccount = await getCurrentAccount(issuerKeyPair);
+    const transaction = new TransactionBuilder(issuerAccount, {
+      fee: fee.toString(),
+      networkPassphrase: Networks.TESTNET,
+    })
+      .addOperation(
+        Operation.payment({
+          destination: keyPair.publicKey(),
+          asset: asset,
+          amount: '1000',
+        })
+      )
+      .setTimeout(30)
+      .build();
+    transaction.sign(issuerKeyPair);
+    const tx = await submitTransaction(transaction);
+    console.log(`Minted, tx=${tx.hash}`);
+  }
+})();
