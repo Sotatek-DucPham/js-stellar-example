f5438d5b777484b42a650a55d59e6a63a31beb65
Sotatek-DucPham <duc.pham@sotatek.com>
2021-07-15 13:43:54 +0700

    feat: offer

---
+++ src/offer.ts
@@ -0,0 +1,32 @@
+import {
+  AuthRequiredFlag,
+  AuthRevocableFlag,
+  Networks,
+  Operation,
+  TransactionBuilder,
+} from 'stellar-sdk';
+import {
+  getCurrentAccount,
+  issuerKeyPair,
+  keyPair,
+  server,
+  submitTransaction,
+} from './common';
+
+(async () => {
+  // query all offers
+  {
+    const offers = await server.offers().limit(100).call();
+    console.log(offers);
+  }
+
+  // query offers of an user
+  {
+    const offers = await server
+      .offers()
+      .forAccount(keyPair.publicKey())
+      .limit(100)
+      .call();
+    console.log(offers);
+  }
+})();
